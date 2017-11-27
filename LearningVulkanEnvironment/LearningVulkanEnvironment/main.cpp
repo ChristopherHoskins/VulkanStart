8471b39d1fd4ab3818ff5db8f704d7b612908719
asdfchris3 <asdfchris3@gmail.com>
2017-11-26 16:02:12 -0800

    Starting Vulkan project from tutorial I found online

--- LearningVulkanEnvironment/LearningVulkanEnvironment/main.cpp
+++ LearningVulkanEnvironment/LearningVulkanEnvironment/main.cpp
@@ -1,45 +1,65 @@
-#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <functional>
 
-class HelloTriangleApplication 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+class HelloTriangleApplication
 {
 public:
   void run() 
   {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
-
 private:
-  void initVulkan() 
+  GLFWwindow* window;
+  void initWindow()
+  {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+  void initVulkan()
   {
 
   }
-
-  void mainLoop() 
+  // Run while checking for events like pressing xuntil the window itself has
+  // been closed
+  void mainLoop()
   {
-
+    while (!glfwWindowShouldClose(window))
+    {
+      glfwPollEvents();
+    }
   }
 
-  void cleanup() 
+  void cleanup()
   {
-
+    glfwDestroyWindow(window);
+    glfwTerminate();
   }
 };
 
-int main() 
+int main()
 {
   HelloTriangleApplication app;
 
-  try 
+  try
   {
     app.run();
   }
-  catch (const std::runtime_error& e) 
+  catch (const std::runtime_error& e)
   {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
