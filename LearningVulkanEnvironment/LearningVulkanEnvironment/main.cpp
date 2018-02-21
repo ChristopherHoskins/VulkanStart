fb1ae01e99b9b9d8c1e75c5641faec091d6e4739
ChrisLeeHoskins <chrisleehoskins@gmail.com>
2018-02-21 15:53:51 -0800

    Command pool & buffer

--- LearningVulkanEnvironment/LearningVulkanEnvironment/main.cpp
+++ LearningVulkanEnvironment/LearningVulkanEnvironment/main.cpp
@@ -315,6 +315,25 @@ private:
 
       VkRenderPassBeginInfo renderPassInfo = {};
       renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+      renderPassInfo.renderPass = renderPass;
+      renderPassInfo.framebuffer = swapChainFramebuffers[i];
+      renderPassInfo.renderArea.offset = { 0,0 };
+      renderPassInfo.renderArea.extent = swapChainExtent;
+
+      VkClearValue clearColor = { 0.0f, 0.0f, 0.0f, 1.0f };
+      renderPassInfo.clearValueCount = 1;
+      renderPassInfo.pClearValues = &clearColor;
+
+      vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+      vkCmdBindPipeline(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+
+      vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+
+      vkCmdEndRenderPass(commandBuffers[i]);
+
+      if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS)
+        throw std::runtime_error("failed to record command buffer!");
     }
   }
 
